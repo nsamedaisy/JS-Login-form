f6b63ed9cfcc733cbc739e9778a67aa9af3717f6
nsamedaisy <nsamedaisy@gmail.com>
2023-04-13 11:31:37 +0100

    JS loginform

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
+var i = document.getElementById("pass");
 function myfunction() {
-    var i = document.getElementById("pass");
-
+    
     if(i.type == "password") {
         i.type = "text";
     }
     else {
         i.type = "password";
     }
-}
-
-var password = document.getElementById("pass");
-var passCheck = document.getElementById("error");
-
-function validate() {
-
-     if (password.value.length <= 8) {
-			// alert("Password too short")
-			passCheck.innerHTML = "password shouldnt be less than 8 characters"
-		}
-		else {
-			passCheck.innerHTML = "Thank you for loging in"
-		}
 }
 
 var loginform = document.getElementById("pop-upform");
@@ -31,4 +17,32 @@ function openForm() {
 
 function closeForm() {
 	loginform.classList.remove("open-form")
+}
+
+// // validation for username
+
+var username = document.getElementById("user");
+var passCheck = document.getElementById("error");
+
+function validate() {
+	if (username.value.length <= 6) {
+		passCheck.innerHTML = "username is invalid"
+	}
+	else {
+		passCheck.innerHTML = "validated"
+	}
+}
+
+// // validation for password
+
+var password = document.getElementById("pass");
+var passCheck = document.getElementById("error");
+
+function validate() {
+     if (password.value.length <= 8) {
+			passCheck.innerHTML = "password is invalid"
+		}
+		else {
+			passCheck.innerHTML = "validated"
+		}
 }
